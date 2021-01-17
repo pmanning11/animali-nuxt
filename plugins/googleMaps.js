f3f67f90d2a73c0954b4f8919212803dd14b4417
PJ Manning <phil.manning11@gmail.com>
2021-01-18 00:06:44 +0800

    changing nuxt config to use process

--- plugins/googleMaps.js
+++ plugins/googleMaps.js
@@ -9,11 +9,13 @@ import Vue from 'vue'
 // Google Maps
 import * as VueGoogleMaps from '@/node_modules/vue2-google-maps'
 Vue.use(VueGoogleMaps, {
-  load: {
-    // Add your API key here
-    key: 'AIzaSyCc4mj6ZFm58kvFIbiUszAI5fAsDAMHBeE',
-    libraries: 'places', // This is required if you use the Auto complete plug-in
-  },
+    load: {
+        // Add your API key here
+        // key: 'AIzaSyCc4mj6ZFm58kvFIbiUszAI5fAsDAMHBeE',
+        // key: process.env.GOOGLE_MAPS_API_KEY,
+        key: $nuxt.$config.googleMapsApiKey,
+        libraries: 'places' // This is required if you use the Auto complete plug-in
+    }
 })
 import GmapCluster from '@/node_modules/vue2-google-maps/dist/components/cluster' // replace src with dist if you have Babel issues
 Vue.component('GmapCluster', GmapCluster)
