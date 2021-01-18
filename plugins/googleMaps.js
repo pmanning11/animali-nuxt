724f722e9fa1558f8a23c5313be792b0a81c39b3
PJ Manning <phil.manning11@gmail.com>
2021-01-18 13:06:24 +0800

    adding google maps

--- plugins/googleMaps.js
+++ plugins/googleMaps.js
@@ -1,19 +1,12 @@
 import Vue from 'vue'
-
 // import * as VueGoogleMaps from "@/node_modules/vue2-google-maps";
-
-// Vue.use(VueGoogleMaps, {
-//   load: { key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY }
-// });
 
 // Google Maps
 import * as VueGoogleMaps from '@/node_modules/vue2-google-maps'
 Vue.use(VueGoogleMaps, {
     load: {
         // Add your API key here
-        // key: 'AIzaSyCc4mj6ZFm58kvFIbiUszAI5fAsDAMHBeE',
-        // key: process.env.GOOGLE_MAPS_API_KEY,
-        key: $nuxt.$config.googleMapsApiKey,
+        key: 'AIzaSyCc4mj6ZFm58kvFIbiUszAI5fAsDAMHBeE',
         libraries: 'places' // This is required if you use the Auto complete plug-in
     }
 })
